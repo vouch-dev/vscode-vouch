86001e263495149ef8e800e35ae6fcd48361b15e
rndhouse <rndhouse@protonmail.com>
2021-03-07 22:52:10 +0000

    Remove make and unmake review primary.

--- src/recorder/commands.ts
+++ src/recorder/commands.ts
@@ -461,31 +461,6 @@ export function registerRecorderCommands() {
   );
 
   vscode.commands.registerCommand(
-    `${EXTENSION_NAME}.makeTourPrimary`,
-    async (node: CodeTourNode) => {
-      const primaryTour = node.tour;
-      primaryTour.isPrimary = true;
-      saveTour(primaryTour);
-
-      store.tours
-        .filter(tour => tour.id !== primaryTour.id && tour.isPrimary)
-        .forEach(tour => {
-          delete tour.isPrimary;
-          saveTour(tour);
-        });
-    }
-  );
-
-  vscode.commands.registerCommand(
-    `${EXTENSION_NAME}.unmakeTourPrimary`,
-    async (node: CodeTourNode) => {
-      const primaryTour = node.tour;
-      delete primaryTour.isPrimary;
-      saveTour(primaryTour);
-    }
-  );
-
-  vscode.commands.registerCommand(
     `${EXTENSION_NAME}.saveTourStep`,
     async (comment: PlayerCodeReviewComment) => {
       if (!comment.parent) {
